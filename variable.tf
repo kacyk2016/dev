8556ac62011237dd2d11599615376f5118352a03
kacyk2016 <94801373+kacyk2016@users.noreply.github.com>
2023-01-18 08:20:32 -0600

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,1 +1,14 @@
+module "tomcat" {
+source  = "terraform-aws-modules/ec2-instance/aws"
+  version = "~> 3.0"
+  name = variable.name_input
 
+  ami                    = variable.my_ami
+  instance_type          = "t2.micro"
+  key_name               = variable.key
+
+  tags = {
+    Terraform   = "true"
+    Environment = "dev"
+  }
+}
