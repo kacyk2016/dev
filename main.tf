359c49e0c0f43a2f6caf4551e2fe85ae282642d5
kacyk2016 <94801373+kacyk2016@users.noreply.github.com>
2023-01-18 08:22:15 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,1 +1,35 @@
+pipeline {
+    tools {
+        terraform 'terraformv1.3.7'
+    }
+    agent any
 
+    stages {
+        stage('Initialization') {
+            steps {
+                sh 'terraform init'
+            }
+        }
+       stage('Validate') {
+            steps {
+                echo 'starting validaion'
+                sh 'date'
+                sh 'terraform validate'
+            }
+        } 
+        stage('Plan') {
+            steps {
+                echo 'planning started'
+                echo 'date'
+                sh 'terraform plan'
+            }
+        }
+        stage('Apply') {
+            steps {
+                echo 'deploying to aws'
+                echo 'date'
+                sh 'terraform destroy --auto-approve'
+            }
+        }
+    }
+}
